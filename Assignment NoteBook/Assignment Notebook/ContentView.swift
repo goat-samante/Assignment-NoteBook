9bf951ad70c77042eadc9f285b88a168d37c017e
Samuel Amante <amantsa76@gmail.com>
2025-02-24 07:32:12 -0600

    added item view

--- Assignment NoteBook/Assignment Notebook/ContentView.swift
+++ Assignment NoteBook/Assignment Notebook/ContentView.swift
@@ -6,49 +6,44 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @ObservedObject var assignmentList = AssignmentList()
+    @State private var showingAddAssignmentView = false
+    
     var body: some View {
-        struct ContentView: View {
-            @ObservedObject var assignmentList = AssignmentList()
-            @State private var showingAddAssignmentView = false
-            
-            var body: some View {
-                NavigationView {
-                    List {
-                        ForEach(assignmentList.items) { item in
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text(item.course).font(.headline)
-                                    Text(item.description)
-                                }
-                                Spacer()
-                                Text(item.dueDate, style: .date)
-                            }
+        NavigationView {
+            List {
+                ForEach(assignmentList.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.course).font(.headline)
+                            Text(item.description)
                         }
-                        .onMove(perform: move)
-                        .onDelete(perform: delete)
-                    }
-                    .navigationBarTitle("Assignments")
-                    .navigationBarItems(leading: EditButton(), trailing: Button(action: {
-                        showingAddAssignmentView = true
-                    }) {
-                        Image(systemName: "plus")
-                    })
-                    .sheet(isPresented: $showingAddAssignmentView) {
-                        AddAssignmentView(assignmentList: assignmentList)
+                        Spacer()
+                        Text(item.dueDate, style: .date)
                     }
                 }
+                .onMove(perform: move)
+                .onDelete(perform: delete)
             }
-            
-            func move(from source: IndexSet, to destination: Int) {
-                assignmentList.items.move(fromOffsets: source, toOffset: destination)
-            }
-            
-            func delete(at offsets: IndexSet) {
-                assignmentList.items.remove(atOffsets: offsets)
+            .navigationBarTitle("Assignments")
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: {
+                showingAddAssignmentView = true
+            }) {
+                Image(systemName: "plus")
+            })
+            .sheet(isPresented: $showingAddAssignmentView) {
+                AddAssignmentView(assignmentList: assignmentList)
             }
         }
+    }
+    
+    func move(from source: IndexSet, to destination: Int) {
+        assignmentList.items.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    func delete(at offsets: IndexSet) {
+        assignmentList.items.remove(atOffsets: offsets)
     }
 }
 
